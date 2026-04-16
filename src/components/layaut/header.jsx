ddb3f5bf559fba7a198fc80aedddf271d16c7179
Cristina <tu-emailejemplo.com>
2026-04-16 14:14:50 +0200

    Add house shields to Header with gold and responsive styles

--- src/components/layaut/header.jsx
+++ src/components/layaut/header.jsx
@@ -1,5 +1,35 @@
-function Header() {
-  return <header className="header"></header>;
-}
+// En tu Header.jsx
+import logoHarry from "../../images/harry.png";
+import escudoGryffindor from "../../images/houses/gryffindor.png";
+import escudoSlytherin from "../../images/houses/slytherin.png";
+import escudoRavenclaw from "../../images/houses/ravenclaw.png";
+import escudoHufflepuff from "../../images/houses/hufflepuff.png";
 
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header__houses">
+        <img src={escudoGryffindor} alt="Gryffindor" className="houses__left" />
+        <img src={escudoSlytherin} alt="Slytherin" className="houses__left" />
+      </div>
+
+      <div className="header__logo">
+        <img
+          src={logoHarry}
+          alt="Censo Mágico de Hogwarts"
+          className="header__img"
+        />
+      </div>
+
+      <div className="header__houses">
+        <img src={escudoRavenclaw} alt="Ravenclaw" className="houses__right" />
+        <img
+          src={escudoHufflepuff}
+          alt="Hufflepuff"
+          className="houses__right"
+        />
+      </div>
+    </header>
+  );
+};
 export default Header;
